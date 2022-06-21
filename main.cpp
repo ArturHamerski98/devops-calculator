db5983b2593a1cb801c34c8b4b7417de53275e86
ArturHamerski98 <arturhamerski98@gmail.com>
2022-06-21 18:25:00 +0200

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include "calculator.hpp"
+#include <stdint.h>
+#include <iostream>
+
+int main()
+{
+    try
+    {
+        int result = calculator::eval("(0 + ~(255 & 1000)*3) / -2");
+        std::cout << result << std::endl;
+    
+        // 64-bit arithmetic
+        int64_t r64 = calculator::eval<int64_t>("2**60");
+        std::cout << r64 << std::endl;
+    }
+    catch (calculator::error& e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
+
+    return 0;
+}
